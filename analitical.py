6fa66538d6a05de5bc72772f82edfb09d307301f
andrew <alh31@wl144.mpipks-dresden.mpg.de>
2018-10-26 18:46:24 +0200

    Update analitical.py

--- analitical.py
+++ analitical.py
@@ -7,7 +7,7 @@ def int_nn_bias(shape):
     count=0
     bias=[]
     for size in shape:
-        lweights=np.zeros(size)
+        lweights=np.ones(size)
         bias.append(lweights)
         count+=count
     return bias
@@ -18,13 +18,31 @@ def fully_conected_network(shape):
     for size in shape:
         
         if count>0.:
-            lweights=np.zeros((size,size_pre))
+            lweights=np.ones((size_pre,size))
             weights.append(lweights)
         count=count+1
         
         size_pre=size
         
     return weights
-shape=[3,2,1,3]
-print int_nn_bias(shape)
-print fully_conected_network(shape)
+
+def activation_func(array):
+    return
+
+def forward(input,weights,shape):
+    if len(input)>shape[0]:
+        print "input wrong shape"
+
+    for layer in weights:
+        print input
+        input=np.matmul(input, layer)
+
+    return input
+
+
+shape=[3,2,1]
+
+bias= int_nn_bias(shape)
+weights= fully_conected_network(shape)
+input=np.array([1.,1.,1.])
+print forward(input,weights,shape)
