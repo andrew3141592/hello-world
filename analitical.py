e612113f4bea71e1e793df074b0cd58c0ee29ed2
andrew <alh31@wl144.mpipks-dresden.mpg.de>
2018-10-26 17:54:33 +0200

    Update analitical.py

--- analitical.py
+++ analitical.py
@@ -2,5 +2,13 @@ import numpy as np
 import scipy
 
 
-def int_nn(shape):
-##Shape is a numpy array of lenght=number of layers of nn
+def int_nn_bias(shape):
+##Shape is an array of lenght=number of layers of nn
+    count=0
+    weights=[]
+    for size in shape:
+        lweights=np.zeros(size)
+        weights.append(lweights)
+        count+=count
+    return weights
+print (int_nn_bias([1,2,1]))
